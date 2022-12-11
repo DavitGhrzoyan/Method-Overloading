1044ae4db2f5bf2d93c12d5553133f4b21778f3e
Davit <ghrzoyandavit@gmail.com>
2022-12-11 13:14:17 +0400

    Add project files.

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        /// <summary>
+        /// Վերադարձնում է 2 ամբողջ թվերի գումարը
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Վերադարձնում է 3 ամբողջ թվերի գումարը
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + b;
+        }
+        /// <summary>
+        /// Վերադարձնում է երկու կոտորակային թվերի գումարը
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            double reult = Sum(2.6, 4.5);
+
+            Console.WriteLine(reult);
+        }
+    }
+}
